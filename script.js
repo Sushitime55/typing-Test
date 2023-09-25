47eccae85055c7dece953440c5da8e039887813b
sushimushi420 <129368091+sushimushi420@users.noreply.github.com>
2023-09-24 22:56:26 -0700

    - added start and stop button functionality - added logic comparison check between sample quote and user input

--- script.js
+++ script.js
@@ -22,12 +22,71 @@ const renderNewQuote = async () => {
 
   // array of characters in the quote
   let arr = quote.split("").map((value) => {
-    //wrap the characters in a span tag (not sure the purpose of this, maybe for comparisons later on? will skip for now)
-    // return "<span class='quote-chars'>" + value + "</span>";
-    return value;
+    //wrap the characters in a span tag for comparison
+    return "<span class='quote-chars'>" + value + "</span>";
   });
 
   quoteSection.innerHTML += arr.join("");
+};
+
+// logic for comparing input words with quote
+userInput.addEventListener("input", () => {
+  let quoteChars = document.querySelectorAll(".quote-chars");
+  // create an array from received span tags
+  quoteChars = Array.from(quoteChars);
+
+  // array of user input characters
+  let userInputChars = userInput.value.split("");
+
+  quoteChars.forEach((char, index) => {
+    // check if quote character = user input character
+    // [index](input character)
+    if (char.innerText == userInputChars[index]) {
+      char.classList.add("success");
+    }
+    // else if user hasn't entered anything or backspaced
+    else if (userInputChars[index] == null) {
+      // remove class if any
+      if (char.classList.contains("success")) {
+        char.classList.remove("success");
+      } else {
+        char.classList.remove("fail");
+      }
+    }
+    // if user enters the wrong character
+    else {
+      // check if fail class already exists
+      if (!char.classList.contains("fail")) {
+        // if not, increment and display  number of mistakes
+        mistakes += 1;
+        char.classList.add("fail");
+      }
+      document.getElementById("mistakes").innerText = mistakes;
+    }
+
+    // check if all characters have been entered correctly
+    let check = quoteChars.every((element) => {
+      return element.classList.contains("success");
+    });
+    // end test if all characters are correct
+    if (check) {
+      displayResult();
+    }
+  });
+});
+
+// start test
+const startTest = () => {
+  mistakes = 0;
+  timer = "";
+  userInput.disabled = false;
+  document.getElementById("start-test").style.display = "none";
+  document.getElementById("stop-test").style.display = "block";
+};
+
+// end test
+const displayResult = () => {
+  document.querySelector(".result").style.display = "block";
 };
 
 // on window load, hide stop button, disable user input for textbox, and grab a random quote to display
