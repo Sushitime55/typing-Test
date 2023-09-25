4c5558835f073dc53a253c9a2614e04c80f05c76
sushimushi420 <129368091+sushimushi420@users.noreply.github.com>
2023-09-24 23:13:20 -0700

    - added timer - added results screen - additional functionality on start and stop buttons

--- script.js
+++ script.js
@@ -80,6 +80,7 @@ const startTest = () => {
   mistakes = 0;
   timer = "";
   userInput.disabled = false;
+  timeReduce();
   document.getElementById("start-test").style.display = "none";
   document.getElementById("stop-test").style.display = "block";
 };
@@ -87,6 +88,35 @@ const startTest = () => {
 // end test
 const displayResult = () => {
   document.querySelector(".result").style.display = "block";
+  clearInterval(timer);
+  document.getElementById("stop-test").style.display = "none";
+  userInput.disabled = true;
+  let timeTaken = 1;
+  if (time != 0) {
+    timeTaken = (60 - time) / 100;
+  }
+
+  document.getElementById("wpm").innerText =
+    (userInput.value.length / 5 / timeTaken).toFixed(2) + "wpm";
+  document.getElementById("accuracy").innerText =
+    Math.round(
+      ((userInput.value.length - mistakes) / userInput.value.length) * 100
+    ) + "%";
+};
+
+// update timer
+function updateTimer() {
+  if (time == 0) {
+    // end test if timer reaches 0
+    displayResult();
+  } else {
+    document.getElementById("timer").innerText = --time + "s";
+  }
+}
+// set timer
+const timeReduce = () => {
+  time = 60;
+  timer = setInterval(updateTimer, 1000);
 };
 
 // on window load, hide stop button, disable user input for textbox, and grab a random quote to display
